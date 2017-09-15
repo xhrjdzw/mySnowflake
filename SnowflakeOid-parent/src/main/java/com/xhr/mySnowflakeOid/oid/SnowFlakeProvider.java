3a86cdfd3b7cc37f6037a9d55b1081dda6f7cae6
xhrjdzw <xhrjdzw@126.com>
2017-09-15 21:25:18 +0800

    增加 详细的注释 添加业务相关性

--- SnowflakeOid-parent/src/main/java/com/xhr/mySnowflakeOid/oid/SnowFlakeProvider.java
+++ SnowflakeOid-parent/src/main/java/com/xhr/mySnowflakeOid/oid/SnowFlakeProvider.java
@@ -9,19 +9,37 @@ import org.slf4j.LoggerFactory;
  */
 public class SnowFlakeProvider implements IOidProvider {
 
+    /** 增加业务相关性  workerId 工作ID (0~31) */
     private final long workerId;
+
+    /** 开始时间截 (2015-01-01) */
     private final static long twepoch = 1456714605277L;
+
+    /** 毫秒内序列(0~4095) */
     private long sequence = 0L;
+
+    /** 机器id所占的位数 */
     private final static long workerIdBits = 6L;
+
+    /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
     public final static long maxWorkerId = -1L ^ -1L << workerIdBits;
+
+    /** 序列在id中占的位数 */
     private final static long sequenceBits = 10L;
 
+    /** 机器ID向左移10位 */
     private final static long workerIdShift = sequenceBits;
+
+    /** 时间截向左移22位(5+5+12) */
     private final static long timestampLeftShift = sequenceBits + workerIdBits;
+
+    /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
     public final static long sequenceMask = -1L ^ -1L << sequenceBits;
 
+    /** 业务ID  **/
     public static final String OID_WORKID = "OID_WORKERID";
 
+    /** 上次生成ID的时间截 */
     private long lastTimestamp = -1L;
 
     private static Logger logger = LoggerFactory.getLogger(SnowFlakeProvider.class);
@@ -53,31 +71,49 @@ public class SnowFlakeProvider implements IOidProvider {
         this.workerId = workerId;
     }
 
+    /**
+     * 线程安全的
+    * */
     public synchronized long nextId() {
+
         long timestamp = this.timeGen();
+
+        //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
+        if (timestamp < lastTimestamp) {
+            throw new RuntimeException(
+                    String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+        }
+
+        //如果是同一时间生成的，则进行毫秒内序列
         if (this.lastTimestamp == timestamp) {
             this.sequence = (this.sequence + 1) & SnowFlakeProvider.sequenceMask;
+            //毫秒内序列溢出
             if (this.sequence == 0) {
+                //阻塞到下一个毫秒,获得新的时间戳
                 logger.info("sequenceMask value is {}." + sequenceMask);
                 timestamp = this.tilNextMillis(this.lastTimestamp);
             }
         } else {
+            //时间戳改变，毫秒内序列重置
             this.sequence = 0;
         }
-        if (timestamp < this.lastTimestamp) {
-            try {
-                throw new Exception(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", this.lastTimestamp - timestamp));
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
-            }
-        }
 
+        //上次生成ID的时间截
         this.lastTimestamp = timestamp;
-        long nextId = ((timestamp - twepoch << timestampLeftShift)) | (this.workerId << SnowFlakeProvider.workerIdShift) | (this.sequence);
-        // logger.info("timestamp:" + timestamp + ",timestampLeftShift:" + timestampLeftShift + ",nextId:" + nextId + ",workerId:" + workerId + ",sequence:" + sequence);
+
+        //移位并通过或运算拼到一起组成64位的ID
+        long nextId = ((timestamp - twepoch << timestampLeftShift))
+                      | (this.workerId << SnowFlakeProvider.workerIdShift)
+                      | (this.sequence);
+        logger.info("timestamp:" + timestamp + ",timestampLeftShift:" + timestampLeftShift + ",nextId:" + nextId + ",workerId:" + workerId + ",sequence:" + sequence);
         return nextId;
     }
 
+    /**
+     * 阻塞到下一个毫秒，直到获得新的时间戳
+     * @param lastTimestamp 上次生成ID的时间截
+     * @return 当前时间戳
+     */
     private long tilNextMillis(final long lastTimestamp) {
         long timestamp = this.timeGen();
         while (timestamp <= lastTimestamp) {
@@ -86,6 +122,10 @@ public class SnowFlakeProvider implements IOidProvider {
         return timestamp;
     }
 
+    /**
+     * 返回以毫秒为单位的当前时间
+     * @return 当前时间(毫秒)
+     */
     private long timeGen() {
         return System.currentTimeMillis();
     }
@@ -97,6 +137,7 @@ public class SnowFlakeProvider implements IOidProvider {
     }
 
     public static void main(String[] args) {
+        System.setProperty("OID_WORKERID", "1");
         SnowFlakeProvider provider = new SnowFlakeProvider();
         String id = provider.generatorID(null);
         logger.info(id);
--- example-xhr-oid/src/test/java/TestIdGenerator.java
+++ example-xhr-oid/src/test/java/TestIdGenerator.java
@@ -7,56 +7,70 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class TestIdGenerator {
+public class TestIdGenerator
+{
 
     public static ApplicationContext context;
 
     @Before
-    public void setUp() throws Exception {
-        context = new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext.xml","classpath:applicationContext-cache.xml","classpath:applicationContext-oid.xml"});
+    public void setUp() throws Exception
+    {
+        context = new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext.xml", "classpath:applicationContext-cache.xml", "classpath:applicationContext-oid.xml"});
         ContextHolder.setContext(context);
     }
-
-    /**
-     * 修改配置文件，设置id生成类型和对应的初始值
-     *
-     * idtype=redis
-     * IUAP_PRIMARY_MYMODULE_START_VALUE=10000
-     */
-//    @Test
-//    public void testRedisGenerator(){
-//        String module = "MYMODULE";
-//        String id = IDGenerator.generateObjectID(module);
-//        System.out.println(id);
-//    }
 
 
     /**
      * 修改配置文件，设置id生成类型
-     *
+     * <p>
      * idtype=snowflake
      */
     @Test
-    public void testSnowFlake(){
+    public void testSnowFlake()
+    {
+
+        /*
+        * 设置指定键对值的系统属性
+        * setProperty (String prop, String value);
+        *
+        * 参数：
+        * prop - 系统属性的名称。
+        * value - 系统属性的值。
+        *
+        * 返回：
+        * 系统属性以前的值，如果没有以前的值，则返回 null。
+        *
+        * 抛出：
+        * SecurityException - 如果安全管理器存在并且其 checkPermission 方法不允许设置指定属性。
+        * NullPointerException - 如果 key 或 value 为 null。
+        * IllegalArgumentException - 如果 key 为空。
+        * 注：这里的system，系统指的是 JRE (runtime)system，不是指 OS。
+        *
+        */
         //OID_WORKERID,系统属性或者环境变量中设置workid
+        //全局变量
         System.setProperty("OID_WORKERID", "1");
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 100000; i++)
+        {
             String id = IDGenerator.generateObjectID(null);
             System.out.println(id);
         }
     }
 
     @Test
-    public void testUapOid(){
+    public void testUapOid()
+    {
         // 八位的字符，与租户方案中的schema命名规范保持一致
         String defautlSchema = "uapcloud";
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             String id = IDGenerator.generateObjectID(defautlSchema);
             System.out.println(id);
         }
         defautlSchema = "tenant01";
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++)
+        {
             String id = IDGenerator.generateObjectID(defautlSchema);
             System.out.println(id);
         }
